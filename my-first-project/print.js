7ded9931f590fc883112e02309f7b024cdaea84d
vitoreticode <vitoreti.code@gmail.com>
2020-10-13 14:35:53 -0300

    first

---
+++ my-first-project/print.js
@@ -0,0 +1,11 @@
+const place = "World"
+const greeting = "Hello"
+
+// Print on console
+console.log(`Hello ${place} 🌎 `)
+// TODO: Add the Earth emoji
+console.log("%s, %s", greeting, place)
+
+/*
+    just a multiline comment, nothing special here
+*/
---
+++ my-first-project/variables.js
@@ -0,0 +1,27 @@
+// There is 3 different ways to declare variables in JS
+
+var one = 1;
+var two = 2;
+const three = 3;
+
+// VAR = Function scoped, can be  changed in scope, avaible before declaration
+// LET = Block scoped, can be changed in scope, only avaible after the declaration
+// CONST = Block scoped, cannot be changed, only avaible before the declaration
+
+// Use const by default
+// Use let in loops
+// var there is no use anymore, prevent bugs
+
+var hello = "Hello";
+console.log(hello);
+
+hello = "Hello World";
+console.log(hello);
+
+if (true) {
+    let world = "Hello World";
+    console.log(world)
+}
+
+const lucas = "Lucas"
+console.log(lucas)
